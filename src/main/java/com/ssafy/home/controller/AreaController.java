41da85d2faf717f8c8e1a8962e8f5e3e6ee3e020
0106koj <jonghwankoh@ajou.ac.kr>
2025-12-11 16:38:44 +0900

    🔧 chore: 스키마 변경 및 파라미터 필수 여부 수정

--- src/main/java/com/ssafy/home/controller/AreaController.java
+++ src/main/java/com/ssafy/home/controller/AreaController.java
@@ -26,23 +26,16 @@ public class AreaController {
     @Operation(summary = "사각형 범위 기반 지역 목록 조회", description = "사각형 좌표 범위에 따른 지역 목록을 조회합니다")
     @GetMapping
     public ResponseEntity<CommonResponse<List<AddressResponse>>> searchAreaAddress(
-            @RequestParam(value = "minLat", required = false) Double minLat,
-            @RequestParam(value = "maxLat", required = false) Double maxLat,
-            @RequestParam(value = "minLng", required = false) Double minLng,
-            @RequestParam(value = "maxLng", required = false) Double maxLng,
-            @RequestParam(value = "scope", required = false) AreaScope scope,
+            @RequestParam(value = "minLat") Double minLat,
+            @RequestParam(value = "maxLat") Double maxLat,
+            @RequestParam(value = "minLng") Double minLng,
+            @RequestParam(value = "maxLng") Double maxLng,
+            @RequestParam(value = "scope") AreaScope scope,
             @RequestParam(value = "minPrice", required = false) Integer minPrice,
             @RequestParam(value = "maxPrice", required = false) Integer maxPrice,
             @RequestParam(value = "minPyung", required = false) Integer minPyung,
             @RequestParam(value = "maxPyung", required = false) Integer maxPyung
             ) {
-        boolean isLatLgnMissing = minLat == null || maxLat == null || minLng == null || maxLng == null;
-
-        if (isLatLgnMissing) {
-            return ResponseEntity.badRequest()
-                    .body(CommonResponse.fail("400", "4개의 좌표(minLat, maxLat, minLng, maxLng)는 모두 입력되어야 합니다."));
-        }
-
         List<AddressResponse> areas = areaService.searchAreaAddress(minLat, maxLat, minLng, maxLng, scope);
         return ResponseEntity.ok(CommonResponse.success(areas));
     }
