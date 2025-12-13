39965e47c2b93ea02de56d0443c4736d87262a6e
0106koj <jonghwankoh@ajou.ac.kr>
2025-12-13 15:20:35 +0900

    ✨ feat(area): XML Mapper에 동적 쿼리 전환 및 필터링 적용

--- src/main/java/com/ssafy/home/mapper/AreaMapper.java
+++ src/main/java/com/ssafy/home/mapper/AreaMapper.java
@@ -6,123 +6,30 @@ import com.ssafy.home.dto.PriceRangeParam;
 import com.ssafy.home.dto.PyungRangeParam;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
-import org.apache.ibatis.annotations.Select;
 
 import java.util.List;
 
 @Mapper
 public interface AreaMapper {
-    // TODO: 33평으로 설정되어있는 Mock data를 실제 평균 가격으로 변경 필요
 
-    @Select("""
-        SELECT
-            dong_code AS dongCode,
-            sido_name AS sidoName,
-            NULL AS gugunName,
-            NULL AS dongName,
-            NULL AS aptSeq,
-            NULL AS aptName,
-            NULL AS aptDong,
-            latitude,
-            longitude,
-            avg_price AS avgPrice,
-            NULL as primaryPyung
-        FROM dongcodes
-        WHERE gugun_name IS NULL
-          AND latitude BETWEEN #{geo.minLat} AND #{geo.maxLat}
-          AND longitude BETWEEN #{geo.minLng} AND #{geo.maxLng}
-        LIMIT 100
-        """)
+    // 시도 단위 조회 (XML Mapper 사용)
     List<AddressResponse> findSidoByBoundingBox(@Param("geo") GeoBoundParam geo,
-                                            @Param("price") PriceRangeParam price);
+                                                 @Param("price") PriceRangeParam price);
 
-    @Select("""
-        SELECT
-            dong_code AS dongCode,
-            sido_name AS sidoName,
-            gugun_name AS gugunName,
-            NULL AS dongName,
-            NULL AS aptSeq,
-            NULL AS aptName,
-            NULL AS aptDong,
-            latitude,
-            longitude,
-            avg_price AS avgPrice,
-            NULL as primaryPyung
-        FROM dongcodes
-        WHERE gugun_name IS NOT NULL AND dong_name IS NULL
-            AND latitude BETWEEN #{geo.minLat} AND #{geo.maxLat}
-            AND longitude BETWEEN #{geo.minLng} AND #{geo.maxLng}
-        LIMIT 100
-        """)
+    // 구군 단위 조회 (XML Mapper 사용)
     List<AddressResponse> findGugunByBoundingBox(@Param("geo") GeoBoundParam geo,
-                                                @Param("price") PriceRangeParam price);
+                                                  @Param("price") PriceRangeParam price);
 
-    @Select("""
-        SELECT
-            dong_code AS dongCode,
-            sido_name AS sidoName,
-            gugun_name AS gugunName,
-            dong_name AS dongName,
-            NULL AS aptSeq,
-            NULL AS aptName,
-            NULL AS aptDong,
-            latitude,
-            longitude,
-            avg_price AS avgPrice,
-            NULL as primaryPyung
-        FROM dongcodes
-        WHERE dong_name IS NOT NULL
-            AND latitude BETWEEN #{geo.minLat} AND #{geo.maxLat}
-            AND longitude BETWEEN #{geo.minLng} AND #{geo.maxLng}
-        LIMIT 100
-        """)
+    // 동 단위 조회 (XML Mapper 사용)
     List<AddressResponse> findDongByBoundingBox(@Param("geo") GeoBoundParam geo,
-                                                @Param("price") PriceRangeParam price);
+                                                 @Param("price") PriceRangeParam price);
 
-    @Select("""
-        SELECT
-            hi.dong_code AS dongCode,
-            sido_name AS sidoName,
-            gugun_name AS gugunName,
-            dong_name AS dongName,
-            apt_seq AS aptSeq,
-            apt_nm AS aptName,
-            NULL AS aptDong,
-            hi.latitude,
-            hi.longitude,
-            hi.avg_price AS avgPrice,
-            33 as primaryPyung
-        FROM houseinfos hi, dongcodes dc
-        WHERE hi.dong_code = dc.dong_code
-            AND hi.latitude BETWEEN #{geo.minLat} AND #{geo.maxLat}
-            AND hi.longitude BETWEEN #{geo.minLng} AND #{geo.maxLng}
-        LIMIT 100
-        """)
+    // 아파트 단위 조회: 대표 평형 기준 필터링 (XML Mapper 사용)
     List<AddressResponse> findAptByBoundingBox(@Param("geo") GeoBoundParam geo,
                                                 @Param("price") PriceRangeParam price,
                                                 @Param("pyung") PyungRangeParam pyung);
 
-    @Select("""
-        SELECT
-            hi.dong_code AS dongCode,
-            sido_name AS sidoName,
-            gugun_name AS gugunName,
-            dong_name AS dongName,
-            hi.apt_seq AS aptSeq,
-            apt_nm AS aptName,
-            ads.apt_dong AS aptDong,
-            ads.latitude,
-            ads.longitude,
-            ads.avg_price AS avgPrice,
-            33 as primaryPyung
-        FROM houseinfos hi, dongcodes dc, apt_dong_stats ads
-        WHERE hi.dong_code = dc.dong_code
-            AND hi.apt_seq = ads.apt_seq
-            AND hi.latitude BETWEEN #{geo.minLat} AND #{geo.maxLat}
-            AND hi.longitude BETWEEN #{geo.minLng} AND #{geo.maxLng}
-        LIMIT 100
-        """)
+    // 아파트 동 단위 조회: 대표 평형 기준 필터링 (XML Mapper 사용)
     List<AddressResponse> findAptDongByBoundingBox(@Param("geo") GeoBoundParam geo,
                                              @Param("price") PriceRangeParam price,
                                              @Param("pyung") PyungRangeParam pyung);
